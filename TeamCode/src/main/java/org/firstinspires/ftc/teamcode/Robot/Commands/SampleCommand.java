e1545baf2b55d23d8ff0f76f8b392501d77787b4
turtle4831 <jakethrun@gmail.com>
2024-09-12 13:15:52 -0500

    added commands folder because it dosent show up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/SampleCommand.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Robot.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+public class SampleCommand extends CommandBase {
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
+    }
+}
