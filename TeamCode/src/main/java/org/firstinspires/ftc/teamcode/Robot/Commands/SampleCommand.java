676609a67449fcbe2998ce619a738aa2541d1a16
turtle4831 <jakethrun@gmail.com>
2024-09-12 13:23:18 -0500

    i made a oopsie :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/SampleCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/SampleCommand.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.Robot.Commands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.Subsystem;
 
 public class SampleCommand extends CommandBase {
+    private Subsystem m_subsystem;
+    public SampleCommand(Subsystem subsystem){
+        this.m_subsystem = subsystem;
+        addRequirements(subsystem);
+    }
     @Override
     public void initialize() {
         super.initialize();
