2d9af73a7bf3ad9f94954c861295f9c73edf9bb2
turtle4831 <jakethrun@gmail.com>
2024-09-18 15:34:31 -0500

    added RedTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Opmodes/Teleop/RedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Opmodes/Teleop/RedTeleop.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.Robot.Opmodes.Teleop;
 
-public class RedTeleop {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot.Hardware;
+
+@TeleOp(name = "\uD83D\uDFE5 Murphy BLUE TELEOP \uD83D\uDFE5")
+public class RedTeleop extends OpMode {
+    private Hardware robot = Hardware.getInstance();
+    @Override
+    public void init() {
+        this.robot.Init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        this.robot =  Hardware.getInstance();
+        this.robot.Loop();
+    }
+
+    public void CreateBindings(){
+
+    }
 }
