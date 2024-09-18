11ebcbbafa7d45d6796c2a86d0af261597d1c58e
Frederick10101 <Madden_stables@Hotmail.com>
2024-09-18 15:54:24 -0500

    added horizontal slide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/LinearSlide.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Robot.Subsystems;
+
+import org.firstinspires.ftc.teamcode.Robot.Hardware;
+
+public class LinearSlide {
+    private final Hardware robot = Hardware.getInstance();
+
+}
