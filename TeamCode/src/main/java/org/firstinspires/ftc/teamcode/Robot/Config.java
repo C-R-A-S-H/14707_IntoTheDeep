3dd8e3d594998e9c370f3da54ffd1bf1df808d32
turtle4831 <jakethrun@gmail.com>
2024-09-19 11:56:55 -0500

    i hate thomas

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Config.java
@@ -3,5 +3,10 @@ package org.firstinspires.ftc.teamcode.Robot;
 public class Config {
     //this is where all pid controllers and values get initialized
     public static final double LENGTH = 16;
-    public static final double WIDTH = 14.45;
+    public static final double WIDTH = 14.358;
+
+
+
+
+
 }
