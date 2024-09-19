7ba3d04849d0c3ce73f8c6f8a8f6412467ef238a
turtle4831 <jakethrun@gmail.com>
2024-09-19 11:07:55 -0500

    added updated width and length measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Config.java
@@ -2,6 +2,6 @@ package org.firstinspires.ftc.teamcode.Robot;
 
 public class Config {
     //this is where all pid controllers and values get initialized
-    public static final double LENGTH = 14;
-    public static final double WIDTH = 14;
+    public static final double LENGTH = 16;
+    public static final double WIDTH = 14.45;
 }
