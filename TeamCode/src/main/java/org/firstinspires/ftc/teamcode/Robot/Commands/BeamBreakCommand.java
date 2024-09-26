a702d2fe0ad557aa14fe45188a29840aea90c389
y33t3r3r33 <dumasdd9@gmail.com>
2024-09-26 18:51:11 -0500

    Started BeamBreakCommand

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/BeamBreakCommand.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Robot.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.BeamBreak;
+
+public class BeamBreakCommand extends CommandBase {
+    private BeamBreak subsystem;
+}
